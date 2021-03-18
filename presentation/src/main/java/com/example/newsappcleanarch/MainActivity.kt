02580e7500302fc70297874c12cb72a5398fc310
developer developer <developermagic7@gmail.com>
2021-03-18 10:15:37 -0700

    first commit

--- presentation/src/main/java/com/example/newsappcleanarch/MainActivity.kt
+++ presentation/src/main/java/com/example/newsappcleanarch/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.plusAssign
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsappcleanarch.databinding.ActivityMainBinding
@@ -15,26 +16,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setUpSaveStateBottomSheet()
 
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun setUpSaveStateBottomSheet(){
         val navController = findNavController(R.id.news_nav_host_fragment)
-
         // get fragment
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.news_nav_host_fragment)!!
-
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.news_nav_host_fragment)  as NavHostFragment
         // setup custom navigator
         val navigator = KeepStateNavigator(this, navHostFragment.childFragmentManager, R.id.news_nav_host_fragment)
         navController.navigatorProvider += navigator
-
         // set navigation graph
         navController.setGraph(R.navigation.nav_graph)
-
         binding.newsBottomNav.setupWithNavController(navController)
-
     }
+
 }
